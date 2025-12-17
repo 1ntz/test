331fd32b349725cbfe7b45d6794eb1c20b9fac15
Khang Hintz <minh.hintz@student.uni-luebeck.de>
2025-12-17 17:42:00 +0100

    cleanup again

--- src/scripts/item.ts
+++
@@ -1,9 +0,0 @@
-export type Item = {
-  id?: string; // Optional, da neue Items keine ID haben
-  title: string; // Titel des Items
-  description: string; // Beschreibung des Items
-  price: number; // Kaufpreis in Euro
-  date: string; // Kaufdatum als ISO-String
-  categories: string[]; // Liste der Kategorien
-  image: string; // Optional: vorerst leerer String
-};
